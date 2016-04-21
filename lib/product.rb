4e530a834b668daf9f7c99a29cda1314560c2070
Carrie Sutherland <cs2984@nyu.edu>
2016-04-21 11:12:06 -0400

    Implement error checking for Duplicate Error

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,35 @@
 class Product
+
+	attr_reader :title, :price, :stock
+
 	@@products = []
 
 	def initialize (options={})
-		@@products << self
+		@title = options[ :title]
+		@orice = options [ :price]
+		@stock = options [ :stock]
+
+		#after we collect information throw it into products
+		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	private
+
+	def self.find_by_title (title)
+		@@products.find { |product| product.title == title}
+	end
+
+	def add_to_products
+		product = self.class.find_by_title(@title)
+		if product 
+			raise DuplicateProductError, "#{title} already exists"
+		else
+			@@products << self 
+		end
 	end
 end
 
